e2c2ed6cb6e2b191c5d3292e96208b9759fa5e1a
kimu_shu <alfvegardrisc@gmail.com>
2015-10-30 22:53:28 +0900

    Add license text

---
+++ js/license.js
@@ -0,0 +1,18 @@
+// ******************************************************************* //
+// PERIDOT Chrome Apps driver - 'Canarium' (version 0.9.9)             //
+// Copyright (C) 2015 @kimu_shu and @s_osafune                         //
+// ------------------------------------------------------------------- //
+// The original version of Canarium (below version 0.9.7) is written   //
+// by @s_osafune and distributed under the following license:          //
+//                                                                     //
+//     Copyright (C) 2014, J-7SYSTEM Works.  All rights Reserved.      //
+//                                                                     //
+// * This module is a free sourcecode and there is NO WARRANTY.        //
+// * No restriction on use. You can use, modify and redistribute it    //
+//   for personal, non-profit or commercial products UNDER YOUR        //
+//   RESPONSIBILITY.                                                   //
+// * Redistributions of source code must retain the above copyright    //
+//   notice.                                                           //
+//                                                                     //
+//         PERIDOT Project - https://github.com/osafune/peridot        //
+// ******************************************************************* //
