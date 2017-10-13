25e493b90d298d00322bdfb8cd41486edcc832ae
kimu_shu <alfvegardrisc@gmail.com>
2017-10-13 11:07:25 +0900

    Add comment

--- src/gen2/util.ts
+++ src/gen2/util.ts
@@ -1,4 +1,8 @@
-
+/**
+ * Promiseからコールバック呼び出しへの変換 (Promise → callback(rejected_reason, fulfilled_value))
+ * @param callback コールバック関数
+ * @param promise コールバックを呼び出すためのPromiseオブジェクト
+ */
 export function invokeCallback<T>(callback: (err: Error, result?: T) => void, promise: Promise<T>): void {
     promise.then(
         (result) => callback(undefined, result),
